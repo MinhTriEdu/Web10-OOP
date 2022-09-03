3a89d23c6076a786464d06a79ec12e3946e5c7d4
MinhTriEdu <nguyenminhtri99.edu@gmail.com>
2022-09-03 22:01:27 +0700

    cap nhat, local

--- danhSachSinhVien.js
+++ danhSachSinhVien.js
@@ -11,11 +11,26 @@ function DanhSachSinhVien() {
         return i;
       }
     }
+    //khong tim thay vi tri nao co masv tim kiem thi tra ve -1
     return -1;
   };
 
   this.xoaNhanVien = function (id) {
+    //xoa phan tu trong mang bang splice, vi tri bat dau la id, va phan tu xoa la 1
     this.list.splice(id, 1);
+  };
+  this.timSinhVienTheoMa = function (id) {
+    for (var i = 0; i < this.list.length; i++) {
+      if (this.list[i].maSV == id) {
+        return this.list[i];
+      }
+    }
+  };
+  this.capNhat = function (sv) {
+    var index = this.timSinhVien(sv.maSV);
+    if (index !== -1) {
+      this.list[index] = sv;
+    }
   };
 }
 
--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function layDuLieuDauVao() {
 
   var sinhVien = new SinhVien(
     maSV,
-    email,
     tenSV,
+    email,
     matKhau,
     ngaySinh,
     khoaHoc,
@@ -30,14 +30,6 @@ function layDuLieuDauVao() {
   return sinhVien;
 }
 
-getEle("btnAdd").addEventListener("click", function () {
-  event.preventDefault();
-  var sinhVien = layDuLieuDauVao();
-
-  dssv.themSinhVien(sinhVien);
-  console.log(dssv.list);
-  taoBang(dssv.list);
-});
 function taoBang(arr) {
   getEle("tbodySinhVien").innerHTML = "";
   var content = "";
@@ -50,7 +42,8 @@ function taoBang(arr) {
     <td>${arr[i].ngaySinh}</td>
     <td>${arr[i].khoaHoc}</td>
     <td>${arr[i].DTB}</td>
-    <td class='btn btn-danger' onclick='deleteStudent(${arr[i].maSV})'>Xóa</td>
+    <td class='btn btn-danger' onclick='deleteStudent("${arr[i].maSV}")'>Xóa</td>
+    <td class='btn btn-success' onclick='pushToForm("${arr[i].maSV}")'>Cập nhật</td>
     </tr>
     `;
   }
@@ -63,6 +56,55 @@ function deleteStudent(id) {
   var index = dssv.timSinhVien(id);
   //ham xoa nhan vien, xoa theo vi tri index tim duoc, neu index ===-1 thi khong xoa
   dssv.xoaNhanVien(index);
+  saveListStudentToLocal();
+   taoBang(dssv.list)
   //sau khi xoa xong can render lai table, goi lai ham taoBang()
   // taoBang(dssv.list);
 }
+function saveListStudentToLocal() {
+  // json.stringify => chuyển đổi kiểu dữ liệu bất kì thành json
+  var arr = JSON.stringify(dssv.list);
+  // localStorage.setItem lưu dữ liệu vào localStorage
+  localStorage.setItem("DSSV", arr);
+}
+
+function getDataFromLocal() {
+  var data = localStorage.getItem("DSSV");
+  // JSON.parse => đổi kiểu json thành kiểu dữ liệu bình thường
+  if (data) dssv.list = JSON.parse(data);
+  //tạo bảng sau khi đã lấy dữ liệu thành công trong local
+  taoBang(dssv.list);
+}
+getDataFromLocal();
+
+function pushToForm(msv) {
+  var sv = dssv.timSinhVienTheoMa(msv);
+console.log(sv);
+  getEle("txtMaSV").value = sv.maSV;
+  getEle("txtTenSV").value = sv.tenSV;
+  getEle("txtEmail").value = sv.email;
+  getEle("txtPass").value = sv.matKhau;
+  getEle("txtNgaySinh").value = sv.ngaySinh;
+  getEle("khSV").value = sv.khoaHoc;
+  getEle("txtDiemToan").value = sv.diemToan;
+  getEle("txtDiemLy").value = sv.diemLy;
+  getEle("txtDiemHoa").value = sv.diemHoa;
+  getEle("btnUpdate").style.display='inline-block'
+}
+
+getEle("btnAdd").addEventListener("click", function () {
+  event.preventDefault();
+  var sinhVien = layDuLieuDauVao();
+  dssv.themSinhVien(sinhVien);
+  taoBang(dssv.list);
+  saveListStudentToLocal();
+});
+getEle("btnUpdate").addEventListener('click',function(){
+  event.preventDefault()
+  var data= layDuLieuDauVao()
+  dssv.capNhat(data)
+  saveListStudentToLocal(dssv.list)
+  taoBang(dssv.list)
+  //reset form
+  getEle('formSV').reset()
+})
